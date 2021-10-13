d28e36b47f8266989764b9bf338dce1e271c5e30
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-10-13 12:23:37 -0400

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -1,4 +1,6 @@
 // Test away!
 test('it returns number 7', () => {
-  
+  // assertions
+  // we assert that something (usually the return value of a function) is somthing
+  expect(true).toBe(true)
 })
