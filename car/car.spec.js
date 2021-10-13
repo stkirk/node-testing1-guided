323e4cca4653c60bca80a13222768c8f6192cb17
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-10-13 12:27:27 -0400

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -2,11 +2,12 @@ function gimmeFive() {
   return 5
 }
 
-test('it returns number 5', () => {
+test('it returns number 5', () => { // the actual test
   // we assert that something (usually the return value of a function) is something
   const actual = gimmeFive()
   const expected = 5
-  // assertion
+  // assertions
   expect(actual).toBe(expected)
-  expect(actual).
+  expect(actual).toBeGreaterThan(4)
+  expect(actual).toBeLessThan(6)
 })
